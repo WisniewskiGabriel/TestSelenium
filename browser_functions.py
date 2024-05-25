7020e88b571236d2ec9a1d13c2fffa8c90082acc
WisniewskiGabriel <gabwisniewski@gmail.com>
2024-05-25 20:50:03 -0300

    encapsulamento do uso do driver em modulo de outro arquivo

--- browser_functions.py
+++ browser_functions.py
@@ -1,3 +1,21 @@
+from prefect import task, flow
+from prefect.blocks.system import String
+import re
+from selenium import webdriver
 
 
+@task(name="Get URL for ACME System 1", log_prints=True)
+def get_acme_url():
+    url_acme_system1 = String.load("acme-system-url")
+    pattern = r"(?<=value\=').+?(?='\)$)"
+    url_acme_system1 = re.search(pattern, str(url_acme_system1)).group()
 
+    return url_acme_system1
+
+
+@task(name="Start browser at ACME")
+def start_browser():
+    url_acme_system1 = get_acme_url()
+    driver = webdriver.Chrome()  # For Chrome
+    driver.get(url_acme_system1)
+    return driver
--- main.py
+++ main.py
@@ -3,24 +3,14 @@ from selenium import webdriver
 from prefect.blocks.system import String
 import re
 import time
-
-
-@task(name="Get URL for ACME System 1", log_prints=True)
-def get_acme_url():
-    url_acme_system1 = String.load("acme-system-url")
-    pattern = r"(?<=value\=').+?(?='\)$)"
-    url_acme_system1 = re.search(pattern, str(url_acme_system1)).group()
-
-    return url_acme_system1
+from browser_functions import get_acme_url, start_browser
 
 
 @flow(name="Selenium with Prefect", log_prints=True)
 def main_fn():
-    url_acme_system1 = get_acme_url()
-    chrome_driver_path = 'chromedriver.exe'
-    driver = webdriver.Chrome()  # For Chrome
-    driver.get(url_acme_system1)
-    time.sleep(5)
+    driver = start_browser()
+    driver.get("https://google.com")
+    time.sleep(4)
 
 
 if __name__ == "__main__":
