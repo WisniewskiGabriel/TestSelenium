8a5d9455b40fdc4842a4b656289cc5aa8d33dc55
WisniewskiGabriel <gabwisniewski@gmail.com>
2024-05-25 21:12:56 -0300

    login started

--- browser_functions.py
+++ browser_functions.py
@@ -2,6 +2,10 @@ from prefect import task, flow
 from prefect.blocks.system import String
 import re
 from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @task(name="Get URL for ACME System 1", log_prints=True)
@@ -18,4 +22,18 @@ def start_browser():
     url_acme_system1 = get_acme_url()
     driver = webdriver.Chrome()  # For Chrome
     driver.get(url_acme_system1)
+    return driver
+
+
+@task(name="Start login on ACME")
+def login_acme():
+    driver = start_browser()
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, "To continue, please authenticate here"))
+        )
+        return True
+    except TimeoutException:
+        print("Timed out waiting for element to load")
+
     return driver
--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from selenium import webdriver
 from prefect.blocks.system import String
 import re
 import time
-from browser_functions import get_acme_url, start_browser
+from browser_functions import login_acme
 
 
 @flow(name="Selenium with Prefect", log_prints=True)
 def main_fn():
-    driver = start_browser()
-    driver.get("https://google.com")
-    time.sleep(4)
+    driver = login_acme()
+    driver.quit()
 
 
 if __name__ == "__main__":
     main_fn()
+c
